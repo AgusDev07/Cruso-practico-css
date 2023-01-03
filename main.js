8a8064cc1a89329cb5db2f52959a0e431248724e
AgusDev07 <lagustinmachado1111@gmail.com>
2023-01-03 10:56:36 -0300

    agregamos el menu mobile

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const DesktopMenu = document.querySelector('.desktop-menu');
+const mobileMenuActivator = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu'); 
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+mobileMenuActivator.addEventListener('click', toggleMobileMenu);
 
 
 function toggleDesktopMenu(){
     DesktopMenu.classList.toggle('inactive');
 } //lo que dice aqui es que cada vez que se le de click al mail en el menu mostrará el menu desktop
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+} //lo que dice aqui es que cada vez que se le de click al mail en el menu mostrará el menu mobile
